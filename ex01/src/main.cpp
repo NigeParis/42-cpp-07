32585b175b8bdc8acdbd509b763ab71a459b2505
NigeParis <nrobinso@student.42.fr>
2025-04-15 09:46:38 +0200

    ex01 done

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -1,35 +1,24 @@
 #include "./include/iter.hpp"
-#include <iostream>
-#include <string>
-
-// Function template definition for iter
-template <typename T, typename F>
-void iter(T* array, std::size_t length, F func(T&)) {
-    for (std::size_t i = 0; i < length; ++i) {
-        func(array[i]); // Apply the function to each element of the array
-    }
-}
-
-// Template function for printing elements
-
-template <typename T> void printElement(const T& element) {
-    std::cout << element << " ";
-}
 
 int main() {
-    // Example with an array of int
-    int intArray[] = {1, 2, 3, 4, 5};
+
+    int intArray[] = {1, 2, 3, 4};
+    char charArray[] = {'A', 'B', 'C', 'D', 'E'};
+    std::string strArray[] = {"Hello", "Second element", "3rd element", "Str array last element"};
+
+    std::cout << "--------------- int Array + 33 in printElement ----------------" << std::endl;
+
     std::size_t intLength = sizeof(intArray) / sizeof(intArray[0]);
-    iter(intArray, intLength, printElement); // Output: 1 2 3 4 5
+    iter(intArray, intLength, printElement);
 
-    std::cout << std::endl; // Newline for clean output
+    std::cout << "--------------- string Array + 33 in printElement -------------" << std::endl;
 
-    // Example with an array of std::string
-    std::string strArray[] = {"Hello", "World", "from", "C++"};
     std::size_t strLength = sizeof(strArray) / sizeof(strArray[0]);
-    iter(strArray, strLength, printElement); // Output: Hello World from C++
+    iter(strArray, strLength, printElement);
 
-    std::cout << std::endl; // Newline for clean output
+    std::cout << "--------------- char Array + 33 in printElement----------------" << std::endl;
+    std::size_t charLength = sizeof(charArray) / sizeof(charArray[0]);
+    iter(charArray, charLength, printElement);
 
     return 0;
 }
