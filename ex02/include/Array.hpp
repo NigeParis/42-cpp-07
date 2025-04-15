bb05260288f1fd17055bd36958b22c3679d040e4
NigeParis <nrobinso@student.42.fr>
2025-04-15 15:34:22 +0200

    WIP - operator []

--- ex02/include/Array.hpp
+++ ex02/include/Array.hpp
@@ -6,13 +6,14 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 10:51:54 by nrobinso          #+#    #+#             */
-/*   Updated: 2025/04/15 15:03:26 by nrobinso         ###   ########.fr       */
+/*   Updated: 2025/04/15 15:30:51 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
-
+#include <cstdlib>
+#include <ctime>
 
 template <typename Template> class Array {
 
@@ -24,7 +25,7 @@ template <typename Template> class Array {
         };
 
         Array(unsigned int n): _myArray(new Template[n]), _size(n) {
-            std::cout << "Array [" << n << "] created" << std::endl;    
+            std::cout << "Array [" << n << "] created" << std::endl; 
             for (unsigned int i = 0; i < n; i++) {
                 _myArray[i] = i + 1;
             }
@@ -41,8 +42,6 @@ template <typename Template> class Array {
                 throw std::out_of_range("std::out_of_range \"Out of limits\"");
             return (_myArray[i]);
         };
-
-
          
         unsigned int size(void) {
           
@@ -58,3 +57,4 @@ template <typename Template> class Array {
         unsigned int _size;
     
 };
+
