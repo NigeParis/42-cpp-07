d467b76e5bae4165986330f256f9adcf0c447ce5
NigeParis <nrobinso@student.42.fr>
2025-04-17 10:13:51 +0200

    clean up files

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "../include/Array.hpp"
-
 #define MAX_VAL 750
+
 int main(int, char**)
 {
     {
@@ -55,6 +55,13 @@ int main(int, char**)
 
     {
         std::cout << " ---------------------- my test main ----------------------" << std::endl;
+        std::cout
+        << "Array<int> a(3);" << std::endl
+        << "Array<int> c(a);" << std::endl
+        << "Array<int> b;" << std::endl
+        << "Array<int> d;" << std::endl
+        << "d = a;" << std::endl
+        << std::endl;
 
         Array<int> a(3);
         Array<int> c(a);
@@ -62,24 +69,26 @@ int main(int, char**)
         Array<int> d;
         
         d = a;
-        std::cout << "Array size is: " << a.size() << std::endl;
         
         try {
+            std::cout << std::endl;
             std::cout << RED << "created a(3)" << RESET << std::endl;
+            std::cout << "Array size of a(3) is: " << a.size() << std::endl;
             std::cout << a[2] << " : is the value of Array a[2]" << std::endl;
             std::cout << BLUE  << "created c(a)" << RESET << std::endl;
             std::cout << c[2] << " : is the value of Array c[2]" << std::endl;
-            std::cout << GREEN << "created a(3) - show value a[0]" << RESET << std::endl;
+            std::cout << GREEN << "show value a[0]" << RESET << std::endl;
             std::cout << a[0] << " : is the value of Array a[0]" << std::endl;
             std::cout << YELLOW << "created d and then assigned d = a - show value a[0]" << RESET << std::endl;
             std::cout << d[0] << " : is the value of Array d[0]" << std::endl;
-            std::cout << PINK << "created b  - show value b[0]" << RESET << std::endl;
+            std::cout << PINK << "created b  - empty array -> show value b[0]" << RESET << std::endl;
             std::cout << b[0] << " : is the value of Array b[0]" << std::endl;
         }
         catch(std::out_of_range &e ) {
             std::cerr << e.what() << std::endl;   
         }
-        std::cout << "delete all Arrays" << std::endl;
+        std::cout << std::endl;
+        std::cout << "Delete all Arrays<int>" << std::endl;
     }
     return 0;
 }
